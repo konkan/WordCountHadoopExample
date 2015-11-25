03f5272b81038dca718fa93ff9263187f9f5cf38
Konrad Karlsson <konrad.karlsson@tele2.com>
2015-11-25 13:37:40 +0100

    Project import.

---
+++ HadoopWordCountExample/AzureStorageInitiator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace HadoopLab
+{
+    /// <summary>
+    /// Used to initialize Azure blob storage with data.
+    /// </summary>
+    public class AzureStorageInitiator
+    {
+        private readonly CloudBlobClient _blobClient;
+        private readonly Regex _storageUriRegex = new Regex(@"\/(?<containerName>[^\/]+)\/(?<fileName>.+)",
+        RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public AzureStorageInitiator(string connectionString)
+        {
+            var cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+
+            _blobClient = cloudStorageAccount.CreateCloudBlobClient();
+        }
+
+        public async Task InitStorageAsync()
+        {
+            //Create "virtual" directories
+            CreateBlockBlob("user/test");
+            CreateBlockBlob("user/test/counter");
+            CreateBlockBlob("user/test/counter/input");
+
+            //Upload test data file
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            var testDataPath = Path.GetDirectoryName(assemblyLocation) + "\\TestData.txt";
+            await UploadAsync("user/user/test/counter/input", testDataPath);
+        }
+
+        private void CreateBlockBlob(string path)
+        {
+            var container = _blobClient.GetContainerReference("user");
+
+            var userBlock = container.GetBlockBlobReference(path);
+            userBlock.Metadata.Add("hdi_isfolder", "hdi_isfolder"); //Maybe not needed
+            userBlock.UploadText(string.Empty);
+        }
+
+        private async Task UploadAsync(string blobDirectoryPath, string filePath)
+        {
+            var blobDirectoryUri = new Uri(_blobClient.StorageUri.PrimaryUri.OriginalString + blobDirectoryPath);
+            var container = GetContainer(blobDirectoryUri);
+
+            if (container == null)
+            {
+                Console.WriteLine("Cannot find container for path " + blobDirectoryUri);
+                return;
+            }
+
+            var blockBlob = GetBlockBlobReference(blobDirectoryUri, filePath, container);
+
+            using (var localFileStream = File.OpenRead(filePath))
+            {
+                await blockBlob.UploadFromStreamAsync(localFileStream);
+            }
+        }
+
+        private CloudBlobContainer GetContainer(Uri fileUri)
+        {
+            var match = _storageUriRegex.Match(fileUri.AbsolutePath);
+
+            if (match.Success == false)
+                throw new Exception("Faulty URI");
+
+            var containerName = match.Groups["containerName"].Value;
+
+            return _blobClient.GetContainerReference(containerName);
+        }
+
+        private static CloudBlockBlob GetBlockBlobReference(Uri blobDirectoryUri, string filePath, CloudBlobContainer container)
+        {
+            var length = ("/" + container.Name + "/").Length;
+            var startPath = blobDirectoryUri.AbsolutePath.Substring(length);
+
+            if (startPath.EndsWith("/") == false)
+                startPath += "/";
+
+            var blockName = startPath + Path.GetFileName(filePath);
+
+            return container.GetBlockBlobReference(blockName);
+        }
+    }
+}
---
+++ HadoopWordCountExample/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HadoopLab;
+using Microsoft.Hadoop.MapReduce;
+using Microsoft.WindowsAzure.Storage;
+
+namespace HadoopWordCountExample
+{
+    /// <summary>
+    /// Simple console application that uses HdInsight(Hadoop) to implement a word counter.
+    /// NOTICE: Do not update following Nuget packages as it will not work with Microsoft.Hadoop.MapReduce.
+    /// - Microsoft.WindowsAzure.ConfigurationManager
+    /// - WindowsAzure.Storage
+    /// </summary>
+    class Program
+    {
+        private static readonly Stopwatch Stopwatch = new Stopwatch();
+
+        static void Main()
+        {
+            //Cluster info
+            var clusterUri = new Uri("{{YOUR CLUSTER URI}}");
+            var clusterUserName = "{{YOUR CLUSTER USER NAME}}";
+            var clusterPassword = "{{YOUR CLUSTER PASSWORD}}";
+            var hadoopUserName = "test/counter";    //This also decides path to job folder(input/output)
+            var container = "user";  //Base directory = container
+
+            //Storage info
+            var storageConnectionString = "{{YOUR STORAGE CONNECTION STRING}}";
+            var cloudStorageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            var storageKey = "{{YOUR STORAGE PRIMARY KEY}}";
+            var storageAccount = cloudStorageAccount.BlobStorageUri.PrimaryUri.Host;
+
+            //Init storage with test data
+            var storageInit = new AzureStorageInitiator(storageConnectionString);
+            storageInit.InitStorageAsync()
+                .GetAwaiter()
+                .GetResult();
+
+            //Connect to cluster
+            var hadoop = Hadoop.Connect(clusterUri, clusterUserName, hadoopUserName, clusterPassword, storageAccount,
+                storageKey, container, true);
+
+            //Execute job
+            Stopwatch.Start();
+            var result = hadoop.MapReduceJob.ExecuteJob<CounterJob>();
+            Stopwatch.Stop();
+
+            //Print job result to console.
+            var jobFailed = result.Info.ExitCode != 0;
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(jobFailed
+                ? $"Failed to run job: {result.Info.StandardError}."
+                : $"Completed job in {Stopwatch.Elapsed.TotalSeconds} seconds.");
+
+            if (jobFailed == false)
+                PrintResultToConsole(hadoop);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Will print the result to console.
+        /// </summary>
+        /// <param name="hadoop"></param>
+        private static void PrintResultToConsole(IHadoop hadoop)
+        {
+            var resultRegex = new Regex("(?<word>[a-zA-Z]+)[^0-9]+(?<count>[0-9]+)", RegexOptions.Compiled);
+            var outputLines = hadoop.StorageSystem.ReadAllLines("output/part-00000");
+
+            var wordCounts = (from l in outputLines
+                             let match = resultRegex.Match(l)
+                             where match.Success
+                             select new { Word = match.Groups["word"].Value, Count = int.Parse(match.Groups["count"].Value) })
+                             .ToList();
+
+            Console.WriteLine("Top 10 used words in \"I have a dream\"");
+            foreach (var wordCount in wordCounts.OrderByDescending(o => o.Count).Take(10))
+            {
+                Console.WriteLine($"{wordCount.Word} - {wordCount.Count}");
+            }
+            Console.WriteLine(Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Will map all words per line, that has 4 character or more.
+        /// </summary>
+        public class CounterMap : MapperBase
+        {
+            private readonly Regex _wordRegex = new Regex("(?<word>[a-zA-Z]{4,})", RegexOptions.Compiled);
+
+            public override void Map(string inputLine, MapperContext context)
+            {
+                //Split up line and validate words with regular expression
+                var wordMatches = inputLine
+                    .Split(' ')
+                    .Select(o => _wordRegex.Match(o));
+                
+                foreach (var wordMatch in wordMatches)
+                {
+                    //Skip invalid words.
+                    if(wordMatch.Success == false)
+                        continue;
+
+                    //Add entry with word as key and "1"(counter) as value
+                    context.EmitKeyValue(wordMatch.Groups["word"].Value, "1");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Will calculate the total count of words, on all lines.
+        /// </summary>
+        public class CounterReduce : ReducerCombinerBase
+        {
+            public override void Reduce(string key, IEnumerable<string> values, ReducerCombinerContext context)
+            {
+                var totalCount = values.Sum(o => int.Parse(o));
+
+                context.EmitKeyValue(key, totalCount.ToString());
+            }
+        }      
+
+        /// <summary>
+        /// Job that will count all words in all files given as input.
+        /// </summary>
+        public class CounterJob : HadoopJob<CounterMap, CounterReduce>
+        {
+            public override HadoopJobConfiguration Configure(ExecutorContext context)
+            {
+                var config = new HadoopJobConfiguration
+                {
+                    InputPath = "input",
+                    OutputFolder = "output",
+                    DeleteOutputFolder = true
+                };
+
+                return config;
+            }
+        }
+    }
+
+}
